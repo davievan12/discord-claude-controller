55c564dccc73c6e406c5c7ed45fcaf14c0366d45
Davi Evangelista <davievangelist@email.com>
2026-03-21 11:43:49 -0300

    feat: complete DiscordClaudeBot implementation

--- src/DiscordClaudeBot.js
+++ src/DiscordClaudeBot.js
@@ -13,6 +13,7 @@ class DiscordClaudeBot{
         })
         this.allowedChanelId = allowedChanelId;
         this.controller = new ClaudeTerminalController(workingDir, 120000);
+        this._registerEvents()
     }
 
     _registerEvents(){
@@ -20,7 +21,7 @@ class DiscordClaudeBot{
         this.client.on(Events.MessageCreate, async (msg) => await this._handleMessage(msg))
     }
 
-    _handleMessage(msg){
+     async _handleMessage(msg){
         let mensagem = msg.content; 
         if (msg.author.bot){
             return 
@@ -35,17 +36,39 @@ class DiscordClaudeBot{
         }
 
         // extrair o prompt rs //
-
         const prompt = mensagem.slice(1).trim();
 
-
+        try{
+            const thinking = await msg.reply('Processando');
+            const resultado = await this.controller.run(prompt)
+            await thinking.delete()
+            const chunks = this._splitMessage(resultado);
+            for (const pedaco of chunks) {
+               await msg.channel.send(pedaco)
+            }
+        }
+        catch (err){
+            await msg.reply(`Erro: ${err.message}`)
+        }
 
 
     }   
-    _splitMessage(){
 
+    _splitMessage(resultado){
+        let text = resultado;
+        const MaxLength = 1900;
+        const chunks1 = [];
+        while (text.length > 0) {
+            chunks1.push(text.slice(0, MaxLength))
+            text = text.slice(MaxLength)
+        }
+        return chunks1
     }
+
+
     start(){
         this.client.login(this.token);
     }
 }
+
+module.exports = DiscordClaudeBot
